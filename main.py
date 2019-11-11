1b9487239140df44141cf590a7308e5bcc73b1c8
Isak-Evaldsson <isak.evaldsson@outlook.com>
2019-11-11 11:44:37 +0100

    Refactoring of main.py.

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-from lexer import Lexer
-from parser import Parser
+from compiler import Compiler
 
 text_input = """
 print(32);
 
 print(32+3);
 """
-
-
-lexer = Lexer().get_lexer()
-tokens = lexer.lex(text_input)
-
-#for token in tokens:
-#    print(token)
-
-pg = Parser()
-pg.parse()
-parser = pg.get_parser()
-parser.parse(tokens).eval()
+Compiler(text_input).eval()
