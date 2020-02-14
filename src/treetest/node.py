c7a48cee8fbb806bdb4f0b9e07e169cdc2ce416f
Isak-Evaldsson <isak.evaldsson@outlook.com>
2020-02-14 16:14:26 +0100

    Removed air.

--- src/treetest/node.py
+++ src/treetest/node.py
@@ -4,10 +4,8 @@ class nodeTree:
 
     def __str__(self, level=0):
         ret = "\t" * level + self.name + "\n"
-
         for child in self.children:
             ret += child.__str__(level + 1)
-
         return ret
 
     def print(self):
